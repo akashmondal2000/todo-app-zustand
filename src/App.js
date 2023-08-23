1230e0581140cb596026c5e32bb3660ea9fcedd6
akashmondal2000 <mondalakash494@gmail.com>
2023-08-23 15:26:12 +0530

    add todo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,41 @@
+import { useState } from "react";
+import useTodoStore from "./TodoStore";
 
 const App=()=> {
+  const todos = useTodoStore((state)=>state.todos);
+  const addTodo = useTodoStore((state)=>state.addTodo);
+  const toggleTodo = useTodoStore((state)=>state.toggleTodo);
+
+  const [newTodoText,setNewTodoText]=useState('');
+
+  const handleAddTodo = ()=>{
+    if(newTodoText.trim() !== ''){
+      addTodo(newTodoText);
+      setNewTodoText('')
+    }
+  }
   return (
     <div className="App">
     <h1>Zustand todo</h1>
+
+    <input type="text"
+    value={newTodoText}
+    onChange={(e)=>setNewTodoText(e.target.value)}
+     />
+     <button onClick={handleAddTodo}>Add Todo</button>
+
+     <ul>
+     {todos.map((todo,index)=>(
+      <li
+      key={index}
+      style={{textDecoration:todo.done? "line-through":"none"}}
+      onClick={()=>toggleTodo(index)}
+      >
+      {todo.text}
+      </li>
+     ))}
+
+     </ul>
 
     </div>
   );
